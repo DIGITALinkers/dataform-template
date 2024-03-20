d4ef87a7fd6348768bf8d3fce9eebb4ea8684232
Nathan SKELTON <nathan.skelton@digitalinkers.com>
2024-03-20 14:08:11 +0000

    declare data source

---
+++ definitions/source/ga4_export.js
@@ -0,0 +1,11 @@
+declare({
+    database: constants.GA4_DATABASE,
+    schema: constants.GA4_DATASET,
+    name: constants.GA4_TABLE,
+});
+
+declare({
+    database: constants.GA4_DATABASE,
+    schema: constants.GA4_DATASET,
+    name: 'events_*',
+});
---
+++ includes/constants.js
@@ -0,0 +1,5 @@
+const GA4_DATABASE = 'projet-pole-dl-dataform';
+const GA4_DATASET = 'test_nathan';
+const GA4_TABLE = 'events_20210131';
+
+module.exports = {GA4_DATABASE, GA4_DATASET, GA4_TABLE}
